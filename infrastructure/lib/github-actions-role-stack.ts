3156a1e50b6a9cbc8704295d6185a0de84aafb04
Dang Nguyen <haidang009@gmail.com>
2025-04-04 02:12:11 +0700

    Remove unecessary files

--- infrastructure/lib/github-actions-role-stack.ts
+++ infrastructure/lib/github-actions-role-stack.ts
@@ -65,7 +65,40 @@ export class GithubActionsRoleStack extends cdk.Stack {
           'ec2:DescribeInternetGateways',
           'ec2:DescribeNatGateways',
           'ec2:DescribeRouteTables',
-          'ec2:DescribeDhcpOptions'
+          'ec2:DescribeDhcpOptions',
+          'ec2:CreateSecurityGroup',
+          'ec2:AuthorizeSecurityGroupIngress',
+          'ec2:AuthorizeSecurityGroupEgress',
+          'ec2:CreateTags',
+          'ec2:DescribeTags'
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add S3 permissions for CDK asset storage
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          's3:CreateBucket',
+          's3:DeleteBucket',
+          's3:PutBucketPolicy',
+          's3:GetBucketPolicy',
+          's3:DeleteBucketPolicy',
+          's3:PutBucketVersioning',
+          's3:GetBucketVersioning',
+          's3:PutBucketPublicAccessBlock',
+          's3:GetBucketPublicAccessBlock',
+          's3:PutObject',
+          's3:GetObject',
+          's3:DeleteObject',
+          's3:PutObjectAcl',
+          's3:GetObjectAcl',
+          's3:ListBucket',
+          's3:GetBucketLocation',
+          's3:GetBucketAcl',
+          's3:PutBucketAcl'
         ],
         resources: ['*'],
       })
@@ -78,27 +111,91 @@ export class GithubActionsRoleStack extends cdk.Stack {
         actions: [
           'ssm:GetParameter',
           'ssm:GetParameters',
-          'ssm:DescribeParameters'
+          'ssm:DescribeParameters',
+          'ssm:PutParameter'
         ],
         resources: ['*'],
       })
     );
 
-    // Add permissions for ECS
+    // Add ECS permissions - needed for Fargate service
     this.role.addToPolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
         actions: [
           'ecs:*',
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add Elastic Load Balancing permissions
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
           'elasticloadbalancing:*',
-          'ec2:CreateSecurityGroup',
-          'ec2:AuthorizeSecurityGroupIngress',
-          'ec2:DescribeSubnets',
-          'ec2:DescribeVpcs',
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add IAM permissions for service roles
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'iam:GetRole',
+          'iam:CreateRole',
+          'iam:DeleteRole',
+          'iam:PassRole',
+          'iam:AttachRolePolicy',
+          'iam:DetachRolePolicy',
+          'iam:PutRolePolicy',
+          'iam:GetRolePolicy',
+          'iam:DeleteRolePolicy',
+          'iam:TagRole'
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add Secrets Manager permissions - needed for database credentials
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'secretsmanager:GetSecretValue',
+          'secretsmanager:DescribeSecret',
+          'secretsmanager:ListSecretVersionIds'
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add Logs permissions
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
           'logs:CreateLogGroup',
           'logs:CreateLogStream',
           'logs:PutLogEvents',
           'logs:DescribeLogGroups',
+          'logs:DescribeLogStreams',
+          'logs:DeleteLogGroup',
+          'logs:DeleteLogStream'
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // Add Application Auto Scaling permissions for ECS service
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'application-autoscaling:*'
         ],
         resources: ['*'],
       })
